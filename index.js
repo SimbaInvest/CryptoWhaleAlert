1a7cdb9ffd667d35ef285c9785160c6f7aa1390c
Simba Invest <simbainvest.pro@gmail.com>
2024-03-07 00:00:25 +0100

    filter transaction by id

--- index.js
+++ index.js
@@ -22,39 +22,57 @@ function numberWithCommas(x) {
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    const channel = client.channels.cache.get(process.env.CHANNEL_ID)
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+
+    var currentId = null;
 
     function getTransactions() {
         request(`https://api.whale-alert.io/v1/transactions?api_key=4dWraprhZ6wONpbymUZsNcucs6KntcDY&min_value=10000000&start=${Math.floor(Date.now() / 1000 - 360)}&limit=1`, function (error, response, body) {
             if (error) {
-                console.log(error)
+                console.log(error);
             }
     
             body = JSON.parse(body);
 
             var fromAddress = null;
             var toAddress = null;
+            var id = body['transactions'][0]['id'];
             var amount = body['transactions'][0]['amount']
             var amount_usd = body['transactions'][0]['amount_usd']
+            var blockchain = body['transactions'][0]['blockchain']
             var symbol = body['transactions'][0]['symbol']
             var fromOwner = body['transactions'][0]['from']['owner']
             var toOwner = body['transactions'][0]['to']['owner']
+
+            if (blockchain !== "ethereum" && blockchain !== "bitcoin" || blockchain !== "tron") {
+                return;
+            }
+
+            if (currentId === id) {
+                return;
+            } else {
+                currentId = id;
+            }
       
             if (fromOwner === "unknown" || fromOwner === undefined) {
                 fromAddress = "unknown wallet";
             }
             else {
-                fromAddress = capitalizeFirstLetter(fromOwner)
+                fromAddress = capitalizeFirstLetter(fromOwner);
             }
       
             if (toOwner === "unknown" || toOwner === undefined) {
                 toAddress = "unknown wallet";
             }
             else {
-                toAddress = capitalizeFirstLetter(toOwner)
+                toAddress = capitalizeFirstLetter(toOwner);
             }
         
-            channel.send("🚨 " + numberWithCommas(Math.round(amount)) + " " + symbol.toUpperCase() + " (" + numberWithCommas(Math.round(amount_usd)) + " USD) transferred from " + fromAddress + " to " + toAddress)
+            if(amount_usd < 10000000){
+                return
+            } else {
+                channel.send("🚨 " + numberWithCommas(Math.round(amount)) + " " + symbol.toUpperCase() + " (" + numberWithCommas(Math.round(amount_usd)) + " USD) transferred from " + fromAddress + " to " + toAddress);
+            }
         });
     }
 
